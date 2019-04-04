e0811c95d0215d2a029b3f712307597bdd6960f1
luzivaldo <luzivaldo.henrique@teccross.com>
2019-04-04 18:53:10 -0300

    editei o material.php

---
+++ sge/salvaTurma.php
@@ -0,0 +1,27 @@
+<?php 
+	include_once 'template/autoload.php';
+
+	$turma = new Turma();
+	$turmaDAO = new TurmaDAO();
+
+	if (isset($_GET['id']) and $_GET['acao'] == 'Deletar') {
+		
+		$turmaDAO->deletar($_GET['id']);
+	}else{
+		$turma->setId($_POST['id']);
+		$turma->setDescricao($_POST['descricao']);
+
+		$acao = $_GET['acao'];
+
+		if ($acao == 'Cadastrar') {
+			$turmaDAO->insereTurma($turma);
+		}
+		else if($acao == 'Editar'){
+			$turmaDAO->alterarTurma($turma);
+		}
+	}
+	$msg = "Executado com sucesso!";
+	$class = "success";
+
+	header("Location: turma.php?msg=$msg&class=$class");
+ ?>
---
+++ sge/template/sidebar.php
@@ -0,0 +1,37 @@
+<!-- Side Navbar -->
+        <nav class="side-navbar">
+          <!-- Sidebar Header-->
+          <div class="sidebar-header d-flex align-items-center">
+          <img src="img/logo.jpg" style="width: 80%">
+           <!--  <div class="avatar"><img src="img/avatar-1.jpg" alt="..." class="img-fluid rounded-circle"></div>
+            <div class="title">
+              <h1 class="h4">Mark Stephen</h1>
+              <p>Web Designer</p>
+            </div> -->
+          </div>
+          <!-- Sidebar Navidation Menus-->
+          <span class="heading">Menu</span>
+          <ul class="list-unstyled">
+            <li class="active"> <a href="principal.php"><i class="icon-home"></i>Principal</a></li>
+            
+            <li><a href="#geral" aria-expanded="false" data-toggle="collapse"> <i class="icon-user"></i>Perfis</a>
+               <ul id="geral" class="collapse list-unstyled">
+                    <li class=""> <a href="professor.php"><i class="icon-list"></i>Professor</a></li>
+                    <li class=""> <a href="aluno.php"><i class="icon-list"></i>Aluno</a></li>
+                    <li class=""> <a href="turma.php"><i class="icon-list"></i>Turma</a></li>
+                    <li class=""> <a href="materia.php"><i class="icon-list"></i>Matérias</a></li>
+                    <li class=""> <a href="admins.php"><i class="icon-list"></i>Admins</a></li>
+              </ul>
+          </li>
+
+            <li><a href="#cadastros" aria-expanded="false" data-toggle="collapse"> <i class="icon-user"></i>Cadastros</a>
+              <ul id="cadastros" class="collapse list-unstyled">
+                <li><a href="#">Professor</a></li>
+                <li><a href="CadastrarAluno.php">Aluno</a></li>
+                <li><a href="CadastrarTurma.php">Turma</a></li>
+                <li><a href="CadastrarMateria.php">Materias</a></li>
+                <li><a href="CadastrarAdmins">Admins</a></li>
+              </ul>
+            </li>
+          </ul>
+        </nav>
