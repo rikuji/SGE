aad46597f97375844b21003d44e981bfbe3f8413
luzivaldo <luzivaldo.henrique@teccross.com>
2019-04-16 20:22:17 -0300

    alterei coiso

--- arq/scriptbd1.0.sql
+++ arq/scriptbd1.0.sql
@@ -18,9 +18,9 @@ USE `sge` ;
 -- Table `sge`.`Disciplina`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `sge`.`Disciplina` (
-  `idDisciplina` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `descricao` VARCHAR(20) NOT NULL,
-  PRIMARY KEY (`idDisciplina`))
+  PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
 
@@ -28,9 +28,9 @@ ENGINE = InnoDB;
 -- Table `sge`.`tipoUsuario`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `sge`.`tipoUsuario` (
-  `idtipoUsuario` INT UNSIGNED NOT NULL,
+  `id` INT UNSIGNED NOT NULL,
   `perfil` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`idtipoUsuario`))
+  PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
 
@@ -38,21 +38,21 @@ ENGINE = InnoDB;
 -- Table `sge`.`Professor`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `sge`.`Professor` (
-  `idProfessor` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `Registro` VARCHAR(45) NOT NULL,
   `idDisciplina` INT UNSIGNED NOT NULL,
   `idTipoUsuario` INT UNSIGNED NOT NULL,
-  PRIMARY KEY (`idProfessor`),
+  PRIMARY KEY (`id`),
   INDEX `fk_Professor_Disciplina1_idx` (`idDisciplina` ASC),
   INDEX `fk_Professor_tipoUsuario1_idx` (`idTipoUsuario` ASC),
   CONSTRAINT `fk_Professor_Disciplina1`
     FOREIGN KEY (`idDisciplina`)
-    REFERENCES `sge`.`Disciplina` (`idDisciplina`)
+    REFERENCES `sge`.`Disciplina` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Professor_tipoUsuario1`
     FOREIGN KEY (`idTipoUsuario`)
-    REFERENCES `sge`.`tipoUsuario` (`idtipoUsuario`)
+    REFERENCES `sge`.`tipoUsuario` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -62,10 +62,10 @@ ENGINE = InnoDB;
 -- Table `sge`.`Turma`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `sge`.`Turma` (
-  `idTurma` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `descricao` VARCHAR(45) NOT NULL,
   `periodo` VARCHAR(10) NOT NULL,
-  PRIMARY KEY (`idTurma`))
+  PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
 
@@ -73,7 +73,7 @@ ENGINE = InnoDB;
 -- Table `sge`.`responsavel`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `sge`.`responsavel` (
-  `idRespLegal` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `nome` VARCHAR(100) NOT NULL,
   `cpf` BIGINT(11) ZEROFILL NOT NULL,
   `email` VARCHAR(100) NOT NULL,
@@ -92,12 +92,12 @@ CREATE TABLE IF NOT EXISTS `sge`.`responsavel` (
   `legal` ENUM('S', 'N') NULL,
   `financeiro` ENUM('S', 'N') NULL,
   `idTipoUsuario` INT UNSIGNED NOT NULL,
-  PRIMARY KEY (`idRespLegal`),
+  PRIMARY KEY (`id`),
   UNIQUE INDEX `cpf_UNIQUE` (`cpf` ASC),
   INDEX `fk_responsavelLegal_tipoUsuario1_idx` (`idTipoUsuario` ASC),
   CONSTRAINT `fk_responsavelLegal_tipoUsuario1`
     FOREIGN KEY (`idTipoUsuario`)
-    REFERENCES `sge`.`tipoUsuario` (`idtipoUsuario`)
+    REFERENCES `sge`.`tipoUsuario` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -107,7 +107,7 @@ ENGINE = InnoDB;
 -- Table `sge`.`Aluno`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `sge`.`Aluno` (
-  `idAluno` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `nome` VARCHAR(100) NOT NULL,
   `cpf` BIGINT(11) ZEROFILL NOT NULL,
   `email` VARCHAR(100) NOT NULL,
@@ -125,18 +125,18 @@ CREATE TABLE IF NOT EXISTS `sge`.`Aluno` (
   `senha` VARCHAR(35) NOT NULL,
   `idTipoUsuario` INT UNSIGNED NOT NULL,
   `idResponsavel` INT UNSIGNED NOT NULL,
-  PRIMARY KEY (`idAluno`),
+  PRIMARY KEY (`id`),
   UNIQUE INDEX `cpf_UNIQUE` (`cpf` ASC),
   INDEX `fk_Aluno_tipoUsuario1_idx` (`idTipoUsuario` ASC),
   INDEX `fk_Aluno_responsavelLegal1_idx` (`idResponsavel` ASC),
   CONSTRAINT `fk_Aluno_tipoUsuario1`
     FOREIGN KEY (`idTipoUsuario`)
-    REFERENCES `sge`.`tipoUsuario` (`idtipoUsuario`)
+    REFERENCES `sge`.`tipoUsuario` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Aluno_responsavelLegal1`
     FOREIGN KEY (`idResponsavel`)
-    REFERENCES `sge`.`responsavel` (`idRespLegal`)
+    REFERENCES `sge`.`responsavel` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -146,18 +146,18 @@ ENGINE = InnoDB;
 -- Table `sge`.`Secretaria`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `sge`.`Secretaria` (
-  `idSecretaria` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `nome` VARCHAR(100) NOT NULL,
   `cpf` BIGINT(11) ZEROFILL NOT NULL,
   `senha` VARCHAR(35) NOT NULL,
   `cargo` VARCHAR(15) NOT NULL,
   `idTipoUsuario` INT UNSIGNED NOT NULL,
-  PRIMARY KEY (`idSecretaria`),
+  PRIMARY KEY (`id`),
   UNIQUE INDEX `cpf_UNIQUE` (`cpf` ASC),
   INDEX `fk_Secretaria_tipoUsuario1_idx` (`idTipoUsuario` ASC),
   CONSTRAINT `fk_Secretaria_tipoUsuario1`
     FOREIGN KEY (`idTipoUsuario`)
-    REFERENCES `sge`.`tipoUsuario` (`idtipoUsuario`)
+    REFERENCES `sge`.`tipoUsuario` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -173,12 +173,12 @@ CREATE TABLE IF NOT EXISTS `sge`.`TurmaDisciplina` (
   INDEX `fk_TurmaDisciplina_Disciplina1_idx` (`idDisciplina` ASC),
   CONSTRAINT `fk_TurmaDisciplina_Turma1`
     FOREIGN KEY (`idTurma`)
-    REFERENCES `sge`.`Turma` (`idTurma`)
+    REFERENCES `sge`.`Turma` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_TurmaDisciplina_Disciplina1`
     FOREIGN KEY (`idDisciplina`)
-    REFERENCES `sge`.`Disciplina` (`idDisciplina`)
+    REFERENCES `sge`.`Disciplina` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -194,12 +194,12 @@ CREATE TABLE IF NOT EXISTS `sge`.`TurmaProfessor` (
   INDEX `fk_TurmaProfessor_Professor1_idx` (`idProfessor` ASC),
   CONSTRAINT `fk_TurmaProfessor_Turma1`
     FOREIGN KEY (`idTurma`)
-    REFERENCES `sge`.`Turma` (`idTurma`)
+    REFERENCES `sge`.`Turma` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_TurmaProfessor_Professor1`
     FOREIGN KEY (`idProfessor`)
-    REFERENCES `sge`.`Professor` (`idProfessor`)
+    REFERENCES `sge`.`Professor` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -215,12 +215,12 @@ CREATE TABLE IF NOT EXISTS `sge`.`AlunoResponsavel` (
   INDEX `fk_Aluno_has_responsavelLegal_Aluno1_idx` (`idAluno` ASC),
   CONSTRAINT `fk_Aluno_has_responsavelLegal_responsavelLegal1`
     FOREIGN KEY (`idResponsavel`)
-    REFERENCES `sge`.`responsavel` (`idRespLegal`)
+    REFERENCES `sge`.`responsavel` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Aluno_has_responsavelLegal_Aluno1`
     FOREIGN KEY (`idAluno`)
-    REFERENCES `sge`.`Aluno` (`idAluno`)
+    REFERENCES `sge`.`Aluno` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -230,30 +230,30 @@ ENGINE = InnoDB;
 -- Table `sge`.`historico`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `sge`.`historico` (
-  `idHistorico` INT UNSIGNED NOT NULL,
+  `id` INT UNSIGNED NOT NULL,
   `nota` DECIMAL(5,2) NOT NULL,
   `falta` INT(3) NOT NULL,
   `idTurma` INT UNSIGNED NOT NULL,
   `idAluno` INT UNSIGNED NOT NULL,
   `idDisciplina` INT UNSIGNED NOT NULL,
-  PRIMARY KEY (`idHistorico`),
-  UNIQUE INDEX `idhistorico_UNIQUE` (`idHistorico` ASC),
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `idhistorico_UNIQUE` (`id` ASC),
   INDEX `fk_historico_Turma1_idx` (`idTurma` ASC),
   INDEX `fk_historico_Aluno1_idx` (`idAluno` ASC),
   INDEX `fk_historico_Disciplina1_idx` (`idDisciplina` ASC),
   CONSTRAINT `fk_historico_Turma1`
     FOREIGN KEY (`idTurma`)
-    REFERENCES `sge`.`Turma` (`idTurma`)
+    REFERENCES `sge`.`Turma` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_historico_Aluno1`
     FOREIGN KEY (`idAluno`)
-    REFERENCES `sge`.`Aluno` (`idAluno`)
+    REFERENCES `sge`.`Aluno` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_historico_Disciplina1`
     FOREIGN KEY (`idDisciplina`)
-    REFERENCES `sge`.`Disciplina` (`idDisciplina`)
+    REFERENCES `sge`.`Disciplina` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
