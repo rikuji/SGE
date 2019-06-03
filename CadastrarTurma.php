bcfd5183354b736cdc74fa11a776c981e4458128
luzivaldohenrique <luzivaldohenrique16@gmail.com>
2019-06-03 18:34:28 -0300

    turma

---
+++ CadastrarTurma.php
@@ -0,0 +1,103 @@
+<?php require_once "template/header.php";
+	  require_once "template/menu.php";		
+ 
+	  $turmaDAO = new TurmaDAO();
+	  $turma = new Turma(); 
+
+if (isset($_GET['idTurma']))
+{ 
+
+    $turma->setIdTurma($_GET['idTurma']);
+
+    $turma = $turmaDAO->procurar($turma->getIdTurma());
+
+    $acao = 'Editar';
+  } else if (isset($_GET['acao']) && $_GET_['acao'] == 'Vizualizar') {
+    
+    $acao = "Vizualizar";
+  } 
+else
+  {
+    $acao = 'Cadastrar';
+
+  }
+
+
+if (isset($_POST['pesquisa'])){
+    $query = $turmaDAO->listar($_POST['pesquisa']);
+  } 
+else
+  {
+        $query = $turmaDAO->listar();
+    }
+?>
+ <div class="content-inner">
+    <!-- Page Header-->
+    <header class="page-header">
+        <div class="container-fluid">
+            <h2 class="no-margin-bottom">.:Formulário de Turma:.</h2>
+        </div>
+    </header>
+
+    
+      <section class="forms"> 
+        <div class="container-fluid">
+
+        <div class="col-lg-12">
+              <?php if(isset($_GET['msg'])){ ?>   
+              <div class="alert alert-<?php echo $_GET['class']; ?>">
+                <?php echo $_GET['msg']; ?>
+              </div>
+              <?php } ?>
+              <div class="cards">
+                  <div class="card-header d-flex align-items-right">
+                    <a href="javascript:history.back(-1)" class="btn btn-warning col-sm-2" style="margin-left: auto;">Voltar</a>
+                  </div>
+              </div>
+            </div>
+
+
+            <!-- Form Elements -->
+            <div class="col-lg-12">
+                <div class="card">
+                    <div class="card-header d-flex align-items-center">
+                        <h3 class="h4">.:Cadastro de turma:.</h3>
+                    </div>
+                    <div class="card-body">
+
+                        <form action="salvaTurma.php?acao=<?php echo $acao; ?>" method="POST" class="form-horizontal">
+
+                         <div class="form-group row">
+                                <label class="col-sm-3 form-control-label">Descrição:</label>
+                                <div class="col-sm-9">
+                                    <input type="text"  name="descricaoTurma" id="descricaoTurma"class="form-control" required autofocus value="<?php echo $turma->getDescricaoTurma() ?>">
+                                    <input type="hidden" name="idTurma" id="idTurma" class="form-control" value="<?php echo $turma->getIdTurma(); ?>">        
+                                </div>
+                            </div>
+                          <div class="form-group row">
+                                <label class="col-sm-3 form-control-label">Periodo:</label>
+                                <div class="col-sm-9">
+                                    <input type="text"  name="periodoTurma" id="periodoTurma" class="form-control" required autofocus value="<?php echo $turma->getPeriodoTurma() ?>">
+                                </div>
+                            </div>
+
+                           
+                            <div class="form-group row">
+                                <div class="col-sm-12 offset-sm-3">
+                                    <button type="submit" class="btn btn-primary"><?php echo $acao; ?></button>
+                                </div>
+                            </div>
+
+                        </form> <!-- fim formulario -->
+                    </div>
+                </div>
+            </div>
+          </div>
+        </div>
+
+  </div>
+</section>
+
+<?php
+include ('template/footer.php');
+?>
--- turma.php
+++ turma.php
@@ -1,8 +1,9 @@
-<?php require_once "template/header.php";
-	  require_once "template/menu.php";	
+<?php 
+    require_once "template/header.php";
+    require_once "template/menu.php";   
  
-	  $turmaDAO = new TurmaDAO();
-	  $turma = new Turma(); 
+    $turmaDAO = new TurmaDAO();
+    $turma = new Turma(); 
 
 if (isset($_GET['idTurma']))
 { 
@@ -31,108 +32,76 @@ else
         $query = $turmaDAO->listar();
     }
 ?>
- <div class="content-inner">
-    <!-- Page Header-->
+<div class="content-inner">
     <header class="page-header">
-        <div class="container-fluid">
-            <h2 class="no-margin-bottom">.:Formulário de Turma:.</h2>
-        </div>
+      <div class="container-fluid">
+        <h2 class="no-margin-bottom">.:Lista de Turmas:.</h2>
+      </div>
     </header>
-
-    
-      <section class="forms"> 
-        <div class="container-fluid">
-
+<div class="panel-body">
+  <section class="tables">
+    <div class="container-fluid">
+      <div class="col-lg-12">
+        <?php if(isset($_GET['msg'])){ ?>
+        <div class="alert alert-<?php echo $_GET['class']; ?>">
+          <?php echo $_GET['msg']; ?>
+        </div>
+        <?php } ?>
+        <div class="cards">
+          <div class="card-header d-flex align-items-rigth" style="border-radius: 8px;">
+            <div class="paenl panel-default">
+              <div class="panel-body">
+                <div class="panel-heading">
+                  <form action="<?php echo $_SERVER['REQUEST_URI'] ?>">
+                    <input type="text" class="form-control col-lg-8" style="float:left; border-radius: 8px;" name="termo" placeholder="Turma" />
+                    &nbsp;
+                    <button class="btn btn-warning" style="background-color: #008fd7;">Buscar</button>
+                  </form>
+                </div>                
+              </div>
+            </div>
+            <a href="javascript:history.back(-1)" class="btn btn-warning col-sm-2" style="margin-left: auto; padding: 15px; background-color: #0b56a6;">Voltar</a>
+          </div>
+        </div>
+      </div>
         <div class="col-lg-12">
-              <?php if(isset($_GET['msg'])){ ?>   
-              <div class="alert alert-<?php echo $_GET['class']; ?>">
-                <?php echo $_GET['msg']; ?>
-              </div>
-              <?php } ?>
-              <div class="cards">
-                  <div class="card-header d-flex align-items-right">
-                    <a href="javascript:history.back(-1)" class="btn btn-warning col-sm-2" style="margin-left: auto;">Voltar</a>
-                  </div>
-              </div>
+          <div class="card">
+            <div class="card-body">
+              <table class="table table-bordered table-striped">
+                <thead>
+                  <tr>
+                    <th>Descrição</th>
+                    <th>Periodo</th>
+                    <th>Ações</th>
+                  </tr>
+                </thead>
+                  <tbody>
+                    <?php foreach ($query as  $turma) {
+                      $temturma = $turmaDAO->listarTurma($turma);
+                   ?>
+                    <tr>
+                      <td><?php echo ($turma->getDescricaoTurma()); ?></td>
+                      <td><?php echo $turma->getPeriodoTurma(); ?></td>
+                      <td>
+                         <a href="#" class="btn btn-info" data-toggle="tooltip" data-placement="top"  title="Editar">
+                           <i class="icon-search" ></i>
+                            </a>
+                            
+                         <a href="" class="btn btn-danger" data-toggle="tooltip" data-placement="top" title="Excluir">
+                           <div data-icon="y" class="icon" ></div>
+                            </a>
+
+                      </td>
+                    <?php } ?>                 
+                    </tr>
+                  </tbody>
+              </table>
             </div>
-
-
-            <!-- Form Elements -->
-            <div class="col-lg-12">
-                <div class="card">
-                    <div class="card-header d-flex align-items-center">
-                        <h3 class="h4">.:Cadastro de turma:.</h3>
-                    </div>
-                    <div class="card-body">
-
-                        <form action="salvaTurma.php?acao=<?php echo $acao; ?>" method="POST" class="form-horizontal">
-
-                         <div class="form-group row">
-                                <label class="col-sm-3 form-control-label">Descrição:</label>
-                                <div class="col-sm-9">
-                                    <input type="text"  name="descricaoTurma" id="descricaoTurma"class="form-control" required autofocus value="<?php echo $turma->getDescricaoTurma() ?>">
-                                    <input type="hidden" name="idTurma" id="idTurma" class="form-control" value="<?php echo $turma->getIdTurma(); ?>">        
-                                </div>
-                            </div>
-                          <div class="form-group row">
-                                <label class="col-sm-3 form-control-label">Periodo:</label>
-                                <div class="col-sm-9">
-                                    <input type="text"  name="periodoTurma" id="periodoTurma" class="form-control" required autofocus value="<?php echo $turma->getPeriodoTurma() ?>">
-                                </div>
-                            </div>
-
-                           
-                            <div class="form-group row">
-                                <div class="col-sm-4 offset-sm-3">
-                                    <button type="submit" class="btn btn-primary"><?php echo $acao; ?></button>
-                                </div>
-                            </div>
-
-                        </form> <!-- fim formulario -->
-                    </div>
-                </div>
-            </div>
-<div class="card-header d-flex align-items-center">
-                        <h3 class="h4">.:Lista de turmas:.</h3>
-                    </div>
-    <div class=" card card-body">
-                <table class="table table-striped table-hover">
-            <thead>
-                          <tr>
-                              <th>Descrição</th>
-                              <th>Periodo</th>
-                              <th>Ação</th>
-                            </tr>
-                        </thead>
-                                 <tbody>
-                          <?php foreach($query AS $turma){  
-                                $temturma = $turmaDAO->listarTurma($turma);
-
-                            ?>
-                            <tr>
-                              <td><?php echo $turma->getDescricaoTurma(); ?></td>
-                              <td><?php echo $turma->getPeriodoTurma(); ?></td>
-                              <td>
-
-                                <a href="novaTurma.php?acao=Editar&idTurma=<?php echo $turma->getIdTurma(); ?>" class="btn btn-warning" data-toggle="tooltip" data-placement="top" title="Editar">
-                                  <i class="fa fa-pencil" aria-hidden="true"></i>
-                                </a>
-                                    <a href="#" class="btn btn-danger" onclick="return excluir('salvaTurma.php?acao=Deletar&idTurma=<?php echo $turma->getIdTurma(); ?>')" data-toggle="tooltip" data-placement="top" title="Deletar">
-                                     <div class="fa fa-trash-o" aria-hidden="true" ></div>
-                                </a>
-                              </td>
-                            </tr>
-                            <?php } ?>
-
-                        </tbody>
-                      </table>
-                    </div>
-                  </div>
-                </div>
-
-  </div>
-</section>
-
-<?php
-include ('template/footer.php');
-?>
+          </div>
+        </div>
+    </div>
+  </section>
+</div>
+ <?php 
+  require_once "template/footer.php";
+  ?>
